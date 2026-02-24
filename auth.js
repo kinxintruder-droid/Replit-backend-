e35a6d1da2b2ad8c62ebda7541c65ea5fa0af726
kinxintruder-droid <kinxintruder@gmail.com>
2026-02-24 16:05:25 +0200

    Add authentication functions for register, login, logout

---
+++ auth.js
@@ -0,0 +1,15 @@
+// js/auth.js
+import { auth } from "./firebase.js";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+export function register(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password);
+}
+
+export function login(email, password) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
+
+export function logout() {
+  return auth.signOut();
+}
